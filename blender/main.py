47f9e59118b81098a1d99e6bf67a47f335dfb527
hanemile <emile.hansmaennel@gmail.com>
2017-09-28 19:36:39 +0200

    implemented a working algroithm until a depth of 2

--- blender/main.py
+++ blender/main.py
@@ -1,45 +1,90 @@
 import numpy as np
 
 # arr_size has to be a positive even integer
-arr_size = 4
+arr_size = 8
 
 # initialize array
 arr = np.zeros((arr_size + 1, arr_size + 1))
 
 # initialize corner values
-arr[0, 0] =                 1
-arr[0, arr_size] =          2
-arr[arr_size, 0] =          1
-arr[arr_size, arr_size] =   1
+arr[0, 0] =                 3
+arr[0, arr_size] =          3
+arr[arr_size, 0] =          3
+arr[arr_size, arr_size] =   3
 
-# def diamond_step():
-#     # find midpoint of the array
-#     mid = int( ( arr_size ) / 2 )
-#
-#     # print the x and y coortinate of the midpoint
-#     print(mid)
-#
-#     # get the corner values
-#     a = arr[mid - mid, mid - mid]
-#     b = arr[mid - mid, mid + mid]
-#     c = arr[mid + mid, mid - mid]
-#     d = arr[mid + mid, mid + mid]
-#
-#     # find the average of the corner values
-#     e = (a + b + c + d) / 4
-#
-#     # assign the median of the corner values to the center field
-#     arr[mid, mid] = e
-#
-# diamond_step()
+# executes the diamond step
+def diamond_step( a, b ):
+    print("a: " + str(a))
+    print("b: " + str(b))
 
-def diamond_step_proto(n, arr_size):
-    n = n * 2
-    mid = int( arr_size / n )
+    # define corner coortinates
+    pa = arr[a[0], a[1]]
+    pb = arr[b[0], b[1]]
+    pc = arr[a[0], b[1]]
+    pd = arr[b[0], a[1]]
 
-    print(mid)
+    print("pa: " + str(a[0]) + " " + str(a[1]) )
+    print("pb: " + str(b[0]) + " " + str(b[1]) )
+    print("pc: " + str(a[0]) + " " + str(b[1]) )
+    print("pd: " + str(b[0]) + " " + str(a[1]) )
 
-diamond_step_proto(1, arr_size)
+    print("")
+
+    # find the middle of the selected field
+    mag = int( ( b[1] - a[1] ) / 2 )
+
+    # assign the average value to the midpoint
+    arr[a[0] + mag , a[1] + mag] = (pa + pb + pc + pd) / 4
+
+# depth 1
+arr_s = int(arr_size)
+diamond_step( [0, 0], [arr_s, arr_s] )
+
+# depth 2
+arr_s2 = int(arr_size / 2)
+diamond_step( [0, 0], [arr_s2, arr_s2] )
+diamond_step( [0, arr_s2], [arr_s2, arr_size] )
+diamond_step( [arr_s2, 0], [arr_size, arr_s2] )
+diamond_step( [arr_s2, arr_s2], [arr_size, arr_size] )
+
+arr_s4 = int(arr_size / 4)
 
 # print the array
-print(arr)
+print("\n" + str(arr))
+
+
+# diamond_step(a, b)
+
+# a O O O O
+# O O O O O
+# O O O O O
+# O O O O O
+# O O O O b
+
+# 1. Find the Midpoint X
+
+# a O O O O
+# O O O O O
+# O O X O O
+# O O O O O
+# O O O O b
+
+# 2. Find the other two points of the rectangle
+
+# a O O O c
+# O O O O O
+# O O X O O
+# O O O O O
+# d O O O b
+
+# 3. Assign X the Value of the average of a, b, c and d
+
+# 4. Define new Values e, f, g and h
+
+# a O f O c
+# O O O O O
+# e O X O g
+# O O O O O
+# d O h O b
+
+# 4. Repeat for aX, fg, Xb and eh
